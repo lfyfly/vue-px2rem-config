44f2410658cdd2db1faa683068c82fdd69372527
lfyfly <410793635@qq.com>
2018-09-03 16:57:27 +0800

    vant 按需引用 px2rem 生效

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vant from 'vant'
-import 'vant/lib/vant-css/index.css'
 
-Vue.use(Vant)
+import { Tabbar, TabbarItem } from 'vant'
+Vue.use(Tabbar).use(TabbarItem)
 
 Vue.config.productionTip = false
 
