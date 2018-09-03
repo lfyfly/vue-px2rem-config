71603810835efaa6146776dca7eff2c03ddea488
lfyfly <410793635@qq.com>
2018-09-03 16:52:25 +0800

    引入vant px转rem 成功

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img class="logo" src="./assets/logo.png">
     <router-view/>
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="shop">标签</van-tabbar-item>
+      <van-tabbar-item icon="chat" dot>标签</van-tabbar-item>
+      <van-tabbar-item icon="records" info="5">标签</van-tabbar-item>
+      <van-tabbar-item icon="gold-coin" info="20">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
